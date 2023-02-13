2d8e791726772603dee2be3be117b0374987de30
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-02-14 01:37:12 +0300

    LocaleResolver config added

---
+++ src/main/java/com/gucardev/translateentity/config/MessageConfig.java
@@ -0,0 +1,29 @@
+package com.gucardev.translateentity.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+  @Bean
+  public AcceptHeaderLocaleResolver localeResolver() {
+    AcceptHeaderLocaleResolver localeResolver =
+        new AcceptHeaderLocaleResolver() {
+          @Override
+          public Locale resolveLocale(HttpServletRequest request) {
+            String locale = request.getParameter("lang");
+            return locale != null
+                ? org.springframework.util.StringUtils.parseLocaleString(locale)
+                : super.resolveLocale(request);
+          }
+        };
+
+    localeResolver.setDefaultLocale(LocaleContextHolder.getLocale());
+    return localeResolver;
+  }
+}
